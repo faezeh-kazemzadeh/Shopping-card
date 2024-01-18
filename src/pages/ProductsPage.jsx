b590a52261e6c6c7abb8bd46535d19a54e5bf775
faezeh-kazemzadeh <faezeh.kazemzadeh@gmail.com>
2024-01-18 12:13:23 +0100

    products page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
-import { useProducts } from '../context/ProductsContext'
+import { useEffect, useState } from "react";
+import { useProducts } from "../context/ProductsContext";
+import Product from "../components/Product";
+import styles from "./style.module.css";
+import Loader from "../components/Loader";
+
+import {
+  searchProducts,
+  filterProducts,
+  createQueryObject,
+  getInitialQuery,
+} from "../helper/helper";
+import { useSearchParams } from "react-router-dom";
+import SearchBox from "../components/SearchBox";
+import SideBar from "../components/SideBar";
 export default function ProductsPage() {
-  const products = useProducts()
+  const products = useProducts();
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    setDisplayed(finalProducts);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
   return (
-    <div>Products</div>
-  )
+    <>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed &&
+            displayed.map((product) => (
+              <Product data={product} key={product.id} />
+            ))}
+        </div>
+        <SideBar setQuery={setQuery} />
+      </div>
+    </>
+  );
 }
