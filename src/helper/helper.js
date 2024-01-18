735efff1c5ed06a170bd3c26550f17f3599e0b79
faezeh-kazemzadeh <faezeh.kazemzadeh@gmail.com>
2024-01-18 12:13:36 +0100

    helper functions

---
+++ src/helper/helper.js
@@ -0,0 +1,44 @@
+export const truncate = (text, length) => {
+  return text.split(" ").slice(0, 3).join("") + "...";
+  // return text.substring(0, length) + "...";
+};
+
+export const searchProducts = (products, search) => {
+  if (!search) return products;
+  const searchedProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search)
+  );
+  return searchedProducts;
+};
+
+export const filterProducts = (products, category) => {
+  if (!category) return products;
+  const filteredProducts = products.filter(
+    (prod) => prod.category === category
+  );
+  return filteredProducts;
+};
+
+export const createQueryObject = (currentQuery, newQuery) => {
+  if (newQuery.category === "all") {
+    const { category, ...rest } = currentQuery;
+    return rest;
+  }
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+  return {
+    ...currentQuery,
+    ...newQuery,
+  };
+};
+
+export const getInitialQuery = (searchParams) => {
+  const query = {};
+  const category = searchParams.get("category");
+  const search = searchParams.get("search");
+  if (category) query.category = category;
+  if (search) query.search = search;
+  return query;
+};
